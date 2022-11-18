a4d4bc57c6eebf7a08863ba5b956dbe48cf2e30e
Rajat <remixrajat1@gmail.com>
2022-11-18 23:57:34 +0530

    added frontend code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [state, setState] = useState("");
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const result = await axios.get(
+        `http://localhost:8000/search/search?searchTerm=${state}`
+      );
+      setData(result.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {error && <p>{error}</p>}
+      <div style={{ margin: "20px" }}>
+        <input
+          type="text"
+          placeholder="Search here"
+          onChange={(event) => {
+            setState(event.target.value);
+          }}
+        />
+        <button
+          style={{ cursor: "pointer", color: "white", backgroundColor: "blue" }}
+          onClick={fetchData}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          margin: "10px",
+        }}
+      >
+        {data.map((d) => (
+          <div
+            style={{
+              display: "flex",
+              height: "500px",
+              width: "200px",
+              flexDirection: "column",
+              backgroundColor: "#CDF9C8",
+              border: "1px solid black",
+              margin: "10px",
+              padding: "10px",
+              overflow: "hidden",
+            }}
+          >
+            <div
+              style={{
+                height: "100px",
+                width: "100%",
+                backgroundColor: "white",
+                overflow: "hidden",
+              }}
+            >
+              <img src={d.imageUrl} alt="" />
+            </div>
+            <h3>{d.headline}</h3>
+            <h3 style={{ color: "red" }}>{d.companies[0].name}</h3>
+            <a style={{ textDecoration: "none" }} href={d.companies[0].url}>
+              {d.CTA}
+            </a>
+            <p>{d.primaryText}</p>
+            <p>{d.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
